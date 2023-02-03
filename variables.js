d7d2a7ed163704d49f1e9f83f7d1f4382c1b3a10
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-03 17:38:38 -0500

    Reassign variables and post outputs

--- variables.js
+++ variables.js
@@ -1,6 +1,21 @@
+//declare string variables
 var frontendLanguage = "Javascript"
 var backendLanguage = "Ruby"
+//declare number variables
 var months = 7
 var mods = 5
+//declare boolean variables
 var tryCoding = false
 var timeZone = false
+
+//reassign variables
+months = 8 
+mods = 6
+tryCoding = true
+timeZone = true
+
+//console log
+console.log("Turing school teaches " + frontendLanguage + " and "+ backendLanguage )
+console.log("It takes about " + months + " months to cover " + mods + " modules .")
+console.log("Requirement to have attended a try coding session? "+ tryCoding)
+console.log("Requirement to be within 2 hours of MT? " + timeZone)
